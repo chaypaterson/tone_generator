d1c774c4a58e2620ae5ce7a1894fe19e602a88ae
Chay Paterson <chaypaterson@gmail.com>
2024-05-29 17:49:03 +0100

    stale comments, code quality

--- audio.c
+++ audio.c
@@ -15,16 +15,10 @@
 
 // define some constants
 
-const double A440 = 2*M_PI*440; // base tone: A 440Hz
-const double Cs5  = 2*M_PI*554.365;
-const double E5   = 2*M_PI*659.255;
+const double A440 = 2 * M_PI * 440; // base tone: A 440Hz
+const double Cs5  = 2 * M_PI * 554.365;
+const double E5   = 2 * M_PI * 659.255;
 const unsigned int DefaultSampleRate = 44100;
-
-// define a header for a wav file
-
-/*typedef struct FileHeader {
-*};
-*/
 
 // define some interesting functions
 
@@ -39,7 +33,7 @@ double sinc(double theta) {
 }
 
 double sinlet(double theta, double decay) {
-    return sin(theta)/cosh(decay*theta);
+    return sin(theta) / cosh(decay * theta);
 }
 
 double besselj(int n, double theta) {
@@ -50,14 +44,11 @@ double besselj(int n, double theta) {
 
 double JSnNorm(double m, double theta) {
     // Jacobi elliptic sn function, with the frequency normalised
-    double result=0;
+    double result = 0;
     double foo,bar;
-    // TODO what if |m|>1?
-    // try to use sn(u,m) = m^-0.5 sn(sqrt(m) u, 1/m) ?
-    // did not work.
     // compute period
-    double K = gsl_sf_hyperg_2F1(0.5,0.5,1,m)*M_PI/2;
-    theta = theta *(2*K)/M_PI;
+    double K = gsl_sf_hyperg_2F1(0.5, 0.5, 1, m) * M_PI / 2;
+    theta = theta * (2 * K) / M_PI;
     // compute result
     gsl_sf_elljac_e(theta, m, &result, &foo, &bar);
     return result;
@@ -65,22 +56,16 @@ double JSnNorm(double m, double theta) {
 
 double legendre(double n, double theta) {
     // legendre polynomials
-    theta = theta/M_PI; //normalise to range 0,1
-    theta = fmod(theta,2.0)-1.0;
+    theta = theta / M_PI; //normalise to range 0,1
+    theta = fmod(theta, 2.0) - 1.0;
     // output value of polynomial:
-    return gsl_sf_legendre_Pl(n,theta);
-}
-
-// TODO mathieu
-double mathieu() {
-    return 0;
+    return gsl_sf_legendre_Pl(n, theta);
 }
 
 // define signal
 double signal(double t) {
     double theta = A440 * t;
-    // TODO pulse width modulation: tune dwell in one region?
-    return JSnNorm(0.95,theta);
+    return JSnNorm(0.95, theta);
 }
 
 int putint(int var, char bytes) {
